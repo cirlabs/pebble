bf05c019dcc87758a0b66d0a2a3311153eeda639
Michael Corey <mikejcorey@gmail.com>
2016-09-20 17:00:20 -0700

    Some actual math.

--- pebble.py
+++ pebble.py
@@ -11,19 +11,26 @@
 # License:     Please see README for the terms under which this
 #              software is distributed.
 #-----------------------------------------------------------------------------
+import math
 
 from miditime.miditime import MIDITime
 
 
 class Pebble(object):
+    '''
+    Lots of stuff cribbed from here: https://www.angio.net/personal/climb/speed
+
+    TODO: Terminal velocity
+    '''
+
     g = 9.8
     mass_grams = 141  # 5 oz, or a baseball
 
     def __init__(self):
         pass
 
-    def drop(self, start_time, mass, height):
-        pass
+    def time_to_splat(self, height_meters):
+        return math.sqrt(2 * height_meters / self.g)
 
 
 if __name__ == "__main__":
